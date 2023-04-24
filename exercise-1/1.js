ce3dc9fd76171535d4692446699b659f1fba969a
Nirmal Khatri <khatrinayana02@gmail.com>
2023-04-25 00:05:37 +0530

    exercise-1

---
+++ exercise-1/1.js
@@ -0,0 +1,39 @@
+var blue = document.getElementById("blue");
+blue.addEventListener("click", buttonBlue);
+
+function buttonBlue() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+
+var red = document.getElementById("red");
+red.addEventListener("click", buttonRed);
+
+function buttonRed() {
+    document.getElementById("box").style.backgroundColor = "red";
+}
+
+
+var black = document.getElementById("black");
+black.addEventListener("click", buttonBlack);
+
+function buttonBlack() {
+    document.getElementById("box").style.backgroundColor = "black";
+}
+
+
+var yellow = document.getElementById("yellow");
+yellow.addEventListener("click", buttonyellow);
+
+function buttonyellow() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+}
+
+
+var green = document.getElementById("green");
+green.addEventListener("click", buttonGreen);
+
+function buttonGreen() {
+    document.getElementById("box").style.backgroundColor = "green";
+}
+
